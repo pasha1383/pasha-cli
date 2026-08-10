2729940a7ee4563c9cd4ba57fd3420e6083c7bfc
pasha1383 <parsashadkam2017@gmail.com>
2026-08-10 09:57:08 +0330

    fix: register kebabCase Handlebars helper for vertical slice template

--- src/core/engine/helpers.js
+++ src/core/engine/helpers.js
@@ -1,6 +1,6 @@
 'use strict';
 const Handlebars = require('handlebars');
-const { pascalCase, camelCase, constantCase, snakeCase } = require('../../utils/strings');
+const { pascalCase, camelCase, constantCase, snakeCase, kebabCase } = require('../../utils/strings');
 
 Handlebars.registerHelper('pascalCase', (str) => pascalCase(str));
 Handlebars.registerHelper('camelCase', (str) => camelCase(str));
@@ -8,6 +8,7 @@ Handlebars.registerHelper('constantCase', (str) => constantCase(str));
 Handlebars.registerHelper('snakeCase', (str) =>
   snakeCase(String(str).split(/[-\s]+/).join('_').toLowerCase())
 );
+Handlebars.registerHelper('kebabCase', (str) => kebabCase(str));
 
 Handlebars.registerHelper('eq', function eq(a, b, options) {
   return a === b ? options.fn(this) : options.inverse(this);
